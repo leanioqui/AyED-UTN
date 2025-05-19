a36af9b01c6791b19a1498a94433c95f3fe4f5e7
Leaniqui <lquintela@frba.utn.edu.ar>
2025-05-18 21:37:19 -0300

    Agrego ejemplos

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -9,6 +9,7 @@ int main() {
   assert(not true == false);
   assert(true and false == false);
   assert(not(true and false) == (not true or not false));  // Ley de De Morgan expresada con boolenos: true == true
+  assert(true or false == true);
 
 
 
@@ -16,6 +17,7 @@ int main() {
   assert('/' + 'O' == 0x7E); // Resultado en hexadecimal
   assert('x' / '<' == 2);
   assert(('m' > 'l') or ('n' < 'a') == true); // Comparación de la posición de los caracteres en ASCII
+  assert('A'<'B'== true); // Combinacion de char con booleano. Comparación de la posición de los caracteres en ASCII
 
 
 
@@ -23,19 +25,23 @@ int main() {
   assert(10u / 2u != 17u);
   assert(200u * 2u == 400u);
   assert(30u > 20u);
+  assert(2u + 7u == 9u);
+
 
 
   // int
   assert(0b1010 + 0b0101 == 15);
   assert(0xA > 0b1001); // 10 > 9
   assert(9 / 5 == 1); // División entre enteros, devuelve la parte entera del resultado
+  assert(24 + 24 != 50);
 
-
+  
   
   // double
   assert(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 != 1.0); // Esto sucede a la imprecisión de los números de punto flotante a la hora de representarlos.
   assert(10.5 / 2.0 == 5.25);
   assert((0.3 - 0.2) != (0.2 - 0.1)); // Ambos deberían ser 0.1, en este caso son valores muy cercanos pero diferentes
+  assert(3.5 or 6.1 > 4.5);
 
 
 
@@ -43,6 +49,6 @@ int main() {
   assert("abc"s + "123"s > "abc122"s);
   assert(("pre"s + "fijo"s).length() == 7);
   assert(""s != "abc"s); // Comparación de cadenas vacías con cadenas no vacías
-
+  assert(("A"s + "B"s + "C"s == "ABC"s));
 
 }
