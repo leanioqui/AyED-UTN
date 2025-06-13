4a097e3df5e81af422f8a1a5133afc4f380c8687
Belmi <bmitetiero@frba.utn.edu.ar>
2025-06-13 19:41:46 -0300

    Declaraciones para tabla

--- 02-Celsius/Temp.cpp
+++ 02-Celsius/Temp.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <cassert>
 #include <cmath>
+#include <iomanip> // Para poder usar std::setw y std::setprecision
 
 bool AreNear(double a, double b, double tolerancia = 0.0001);
 double Celsius (double fahrenheit);
 double Fahrenheit (double celsius);
+void TablaCelsiusAFahrenheit();
+void TablaFahrenheitACelsius();
+
 
 int main() {
     return 0;
@@ -22,4 +26,10 @@ double Celsius (double fahrenheit) {
 
 double Fahrenheit (double celsius) {
     return (celsius * 9.0 / 5.0) + 32.0; // Convierte Celsius a Fahrenheit
+}
+
+void TablaCelsiusAFahrenheit(){
+}
+
+void TablaFahrenheitACelsius(){
 }
