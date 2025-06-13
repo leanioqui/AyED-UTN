2e88f4bc64641386e0e0ad5b67b7ca826de6f4ee
Belmi <bmitetiero@frba.utn.edu.ar>
2025-06-13 18:52:39 -0300

    funciones tp2

---
+++ 02-Celsius/Temp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cassert>
+#include <cmath>
+
+bool AreNear(double a, double b, double tolerancia = 0.0001);
+double Celsius (double fahrenheit);
+double Fahrenheit (double celsius);
+
+int main() {
+    return 0;
+}
+
+
+
+bool AreNear(double a, double b, double tolerancia) {
+    return std::abs(a - b) <= tolerancia; // Compara el valor absoluto de la diferencia entre a y b, con la tolerancia 0.0001
+}
+
+double Celsius (double fahrenheit) {
+    return (fahrenheit - 32.0) * 5.0 / 9.0; // Convierte Fahrenheit a Celsius
+}
+
+double Fahrenheit (double celsius) {
+    return (celsius * 9.0 / 5.0) + 32.0; // Convierte Celsius a Fahrenheit
+}
