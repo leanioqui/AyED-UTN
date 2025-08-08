4b9f151cd04b8e5e1768658e7053ed56343e5bb6
Leaniqui <lquintela@frba.utn.edu.ar>
2025-08-08 20:56:27 -0300

    Actualizo Funcionbart

--- 03-Repeticion/FraseBart.cpp
+++ 03-Repeticion/FraseBart.cpp
@@ -7,6 +7,9 @@ using namespace std::string_literals;
 string frase(unsigned int n);
 
 int main(){
+    assert(frase(0)==""s);
+    assert(frase(3)=="Voy a estudiar programacion \nVoy a estudiar programacion \nVoy a estudiar programacion \n"s);
+    std::cout << frase(0);
     return 0;
 }
 
---
+++ 03-Repeticion/FuncionRecursiva.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+using std::string;
+using namespace std::string_literals;
