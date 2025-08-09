6b8eb16090afb155ba62d6fb8f332fc39f0bec5d
Belmi <bmitetiero@frba.utn.edu.ar>
2025-08-08 22:00:26 -0300

    Agregado estructura de segunda recursividad

--- 03-Repeticion/FuncionRecursiva.cpp
+++ 03-Repeticion/FuncionRecursiva.cpp
@@ -5,15 +5,30 @@ using std::string;
 using namespace std::string_literals;
 
 unsigned int sumaDigitos(unsigned int n);
+void pruebasSumaDigitos();
+unsigned int contarVocales(string s, unsigned int i);
+void pruebasContarVocales();
 
 int main(){
-    assert(sumaDigitos(103)==4);
-    assert(sumaDigitos(0)==0);
-    assert(sumaDigitos(9)==9);
-    assert(sumaDigitos(1358)==17);
+    pruebasSumaDigitos();
+    pruebasContarVocales();
     return 0;
 }
 
 unsigned int sumaDigitos(unsigned int n){
     return n<10?n:(n%10)+sumaDigitos(n/10);
+}
+
+unsigned int contarVocales(string s, unsigned int i) {
+    return 0;
+}
+
+void pruebasSumaDigitos(){
+    assert(sumaDigitos(103)==4);
+    assert(sumaDigitos(0)==0);
+    assert(sumaDigitos(9)==9);
+    assert(sumaDigitos(1358)==17);
+}
+
+void pruebasContarVocales(){
 }
