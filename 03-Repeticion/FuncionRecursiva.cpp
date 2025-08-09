ec9fab4664025e2ef2bdea0aeeae85a493b9c030
Nicolas Paredes <nicolasparedes@frba.utn.edu.ar>
2025-08-08 21:18:26 -0300

    [TP3] Implementacion de la funcion recursiva
    Se defina la implementación de la funcion que suma todos los digitos de un numero entero positivo.

--- 03-Repeticion/FuncionRecursiva.cpp
+++ 03-Repeticion/FuncionRecursiva.cpp
@@ -5,3 +5,11 @@ using std::string;
 using namespace std::string_literals;
 
 unsigned int sumaDigitos(unsigned int n);
+
+int main(){
+    return 0;
+}
+
+unsigned int sumaDigitos(unsigned int n){
+    return n<10?n:(n%10)+sumaDigitos(n/10);
+}
