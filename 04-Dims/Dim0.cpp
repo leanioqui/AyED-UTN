0bf0411fec8e2e4ecdcee093140688594f1ac7a3
Belmi <bmitetiero@frba.utn.edu.ar>
2025-10-08 11:39:21 -0300

    dim0 + test0

--- 04-Dims/Dim0.cpp
+++ 04-Dims/Dim0.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-#include <cassert> 
+#include <cassert>
 using std::cin;
 using std::cout;
+using std::istream;
+
+int ventasTotales (istream& flujo){
+    int total = {};
+    for (int importe; flujo >> importe;){
+        total += importe;
+    }
+    return total;
+}
 
 int main(){
-    
+    int total = ventasTotales(cin);
+    cout << "El total de ventas es: " << total << '\n';
     return 0;
 }
+
+/*  Ejecutar desde la terminal powershell estando en la carpeta 04-Dims:
+    g++ Dim0.cpp -o Dim0.exe
+    Get-Content Test0.txt | .\Dim0 
+*/
